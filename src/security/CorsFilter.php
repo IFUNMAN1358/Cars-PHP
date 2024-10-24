7e159e70331d9ae8137cc46eb267e0a89f96ae1d
IFUNMAN <www.vadimka20058@gmail.com>
2024-10-24 14:46:02 +0300

    add htaccess

--- src/security/CorsFilter.php
+++ src/security/CorsFilter.php
@@ -9,6 +9,7 @@ class CorsFilter {
         header("Access-Control-Allow-Origin: http://localhost:7000");
         header("Access-Control-Allow-Methods: GET, POST, PUT, PATCH, DELETE, OPTIONS");
         header("Access-Control-Allow-Headers: Content-Type, Authorization");
+        header("Access-Control-Allow-Credentials: true");
 
         if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
             http_response_code(200);
